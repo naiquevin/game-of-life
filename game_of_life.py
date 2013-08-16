32ced92c800536108ad98ce1ec29847ef0d0ee9f
Vineet <naikvin@gmail.com>
2013-08-17 00:01:40 +0530

    Fix an major flaw

--- game_of_life.py
+++ game_of_life.py
@@ -45,16 +45,17 @@ def load_grid(filename):
     """Reads a .cells file and loads a Grid namedtuple"""
     name = os.path.splitext(os.path.basename(filename))[0]
     with open(os.path.abspath(filename)) as f:
-        lines = [[1 if c == 'O' else 0 for c in line]
+        lines = [[ALIVE if c == 'O' else DEAD for c in line if c not in ('', '\n')]
                  for line in f
-                 if not line.startswith('!')]
+                 if not line.startswith('!') and line != '']
         rows = len(lines)
         cols = len(lines[0])
         cells = reduce(lambda x, y: x + y, lines, [])
+        padded_cells, (new_rows, new_cols) = pad_cells(cells, size=(rows, cols), padding=1)
         return Grid(name=name,
-                    cells=cells,
-                    rows=rows,
-                    cols=cols)
+                    cells=padded_cells,
+                    rows=new_rows,
+                    cols=new_cols)
 
 
 def make_grid(name, size, live_cells):
@@ -74,6 +75,56 @@ def make_grid(name, size, live_cells):
                 cells=cells,
                 rows=rows,
                 cols=cols)
+
+
+def pad_cells(cells, size, padding=1):
+    rows, cols = size
+    # convert list to matrix
+    matrix = list_to_matrix(cells, size)
+    # pad with extra rows and cols
+    matrix = [[DEAD]*padding + row + [DEAD]*padding
+              for row in matrix]
+    matrix = [[DEAD]*len(matrix[0])] + matrix + [[DEAD]*len(matrix[0])]
+    # convert to flat list again
+    return (matrix_to_list(matrix), (rows+2*padding, cols+2*padding))
+
+
+def test_pad_cells():
+    c1 = range(16)
+    c1_p, (nr, nc) = pad_cells(c1, size=(4, 4))
+    assert nr == 6
+    assert nc == 6
+    assert len(c1_p) == 36
+    assert c1_p == [0,  0,  0,  0,  0, 0,
+                    0,  0,  1,  2,  3, 0,
+                    0,  4,  5,  6,  7, 0,
+                    0,  8,  9, 10, 11, 0,
+                    0, 12, 13, 14, 15, 0,
+                    0,  0,  0,  0,  0, 0]
+
+
+def list_to_matrix(items, size):
+    rows, cols = size
+    return [items[i:i+cols] for i in range(0, len(items), cols)]
+
+
+def test_list_to_matrix():
+    c1 = range(4*4)
+    assert list_to_matrix(c1, (4, 4)) == [[ 0,  1,  2,  3],
+                                          [ 4,  5,  6,  7],
+                                          [ 8,  9, 10, 11],
+                                          [12, 13, 14, 15]]
+
+
+def matrix_to_list(matrix):
+    return reduce(lambda x,y: x+y, matrix, [])
+
+def test_matrix_to_list():
+    m1 = [[ 0,  1,  2,  3],
+          [ 4,  5,  6,  7],
+          [ 8,  9, 10, 11],
+          [12, 13, 14, 15]]
+    assert matrix_to_list(m1) == range(4*4)
 
 
 def update_cells(grid, cells):
@@ -107,9 +158,7 @@ def tick(grid):
     """
     cell_destiny = destiny(grid)
     size = grid.rows * grid.cols
-    new_cells = [cell_destiny(i, s)
-                 for i, s
-                 in zip(range(size), grid.cells)]
+    new_cells = [cell_destiny(i) for i in range(size)]
     return update_cells(grid, new_cells)
 
 
@@ -122,24 +171,47 @@ def destiny(grid):
     the cell
 
     """
-    def inner(i, s):
+    def inner(i):
         pos = id_to_pos(i, grid)
         live_ngh = len(live_neighbours(pos, grid))
-        if s == ALIVE:
+        if grid.cells[i] == ALIVE:
             return ALIVE if live_ngh in (2, 3) else DEAD
-        if s == DEAD:
+        if grid.cells[i] == DEAD:
             return ALIVE if live_ngh == 3 else DEAD
     return inner
 
 
-def print_grid(grid):
+def test_destiny():
+    cells = [
+        0, 0, 0, 0, 0, 0,
+        0, 0, 1, 1, 1, 0,
+        0, 1, 1, 1, 0, 0,
+        0, 0, 0, 0, 0, 0,
+    ]
+    new_cells = [
+        0, 0, 0, 1, 0, 0,
+        0, 1, 0, 0, 1, 0,
+        0, 1, 0, 0, 1, 0,
+        0, 0, 1, 0, 0, 0,
+    ]
+    grid = Grid(name='toad',
+                cells=cells,
+                rows=4,
+                cols=6)
+    dest = destiny(grid)
+    for i in range(grid.rows*grid.cols):
+        assert dest(i) == new_cells[i]
+
+
+def print_grid(grid, init=False):
     """Prints the grid in the terminal and makes sure it's overwritten
 
     :param grid: Grid namedtuple
 
     """
-    for i in range(grid.rows):
-        print(CURSOR_UP_ONE + ERASE_LINE + CURSOR_UP_ONE)
+    if not init:
+        for i in range(grid.rows):
+            print(CURSOR_UP_ONE + ERASE_LINE + CURSOR_UP_ONE)
     gs = [ALIVE_CHAR if c == ALIVE else DEAD_CHAR
           for c in grid.cells]
     output = []
@@ -152,7 +224,7 @@ def print_grid(grid):
     sys.stdout.flush()
 
 
-def loop(state, fn, interval=1):
+def loop(state, fn, interval=1, init=False):
     """Runs infinitely calling the function at the interval and updating
     state as per the function calls return value
 
@@ -161,7 +233,7 @@ def loop(state, fn, interval=1):
     :param interval : refresh rate in seconds
 
     """
-    print_grid(state)
+    print_grid(state, init)
     new_state = fn(state)
     time.sleep(interval)
     return loop(new_state, fn, interval)
@@ -176,7 +248,7 @@ def id_to_pos(cell_index, grid):
 
     """
     row = cell_index//grid.cols
-    column = cell_index % grid.rows
+    column = cell_index % grid.cols
     return (row, column)
 
 
@@ -185,6 +257,16 @@ def test_id_to_pos():
     assert id_to_pos(0, grid) == (0, 0)
     assert id_to_pos(24, grid) == (4, 4)
     assert id_to_pos(10, grid) == (2, 0)
+    toad = Grid(name='toad',
+                cells=[
+                    0, 0, 0, 0, 0, 0,
+                    0, 0, 1, 1, 1, 0,
+                    0, 1, 1, 1, 0, 0,
+                    0, 0, 0, 0, 0, 0,
+                ],
+                rows=4,
+                cols=6)
+    assert id_to_pos(7, toad) == (1, 1)
 
 
 def pos_to_id(pos, grid):
@@ -249,8 +331,8 @@ def test_neighbors():
     assert get_neighbours(p1) == [(0, 1), (1, 0), (1, 1)]
     p2 = id_to_pos(18, grid)
     assert get_neighbours(p2) == [(2, 2), (2, 3), (2, 4),
-                              (3, 2),         (3, 4),
-                              (4, 2), (4, 3), (4, 4)]
+                                  (3, 2),         (3, 4),
+                                  (4, 2), (4, 3), (4, 4)]
     p3 = id_to_pos(24, grid)
     assert get_neighbours(p3) == [(3, 3), (3, 4), (4, 3)]
 
@@ -270,7 +352,7 @@ def live_neighbours(pos, grid):
 
 def main(grid):
     try:
-        loop(grid, tick)
+        loop(grid, tick, interval=0.2, init=True)
     except KeyboardInterrupt:
         print '\rbye!'
         exit(0)
@@ -286,4 +368,3 @@ if __name__ == '__main__':
         grid = load_grid(cellsfile)
         raw_input('Randomly selected to play pattern %r\nPress enter to start' % (grid.name,))
     main(grid)
-
